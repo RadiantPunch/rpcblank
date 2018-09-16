4899b2ff3a8050044930f46faf497084ac612a60
Becky Hile <becky@radiantpunch.com>
2018-09-16 11:01:34 -0400

    Update search highlight function name

--- search-result.php
+++ search-result.php
@@ -2,6 +2,6 @@
 
 <article class="search-result">
 	<h2><?php rpcblank_highlight_search_term( 'title' ); ?></h2>
-	<div class="excerpt"><?php rpcblank_highlight_search_term( 'excerpt' ); ?></div>
+	<div class="excerpt"><?php rpcblank_search_keyphrase_highlight( 'excerpt' ); ?></div>
 	<?php wp_link_pages(); ?>
 </article>
